4bd000472b1a76bce5493e714c2964afc9dae09e
EZENGOLD <noudekiki@gmail.com>
2021-11-03 14:19:13 +0100

    traces view added

---
+++ src/ui/TraceCellRenderer.java
@@ -0,0 +1,21 @@
+package ui;
+
+import java.awt.*;
+
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.TableCellRenderer;
+
+public class TraceCellRenderer extends Button implements TableCellRenderer {
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		setHorizontalAlignment(SwingConstants.LEFT);
+		setText(value != null && !((String) value.toString()).isEmpty() ? value.toString() : "Aucune trace");
+		setBackground(isSelected ? Color.decode("#9099ae") : new Color(0, 0, 0, 0));
+
+		return this;
+	}
+
+}
---
+++ src/ui/TracesTableModel.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.table.DefaultTableModel;
+
+public class TracesTableModel extends DefaultTableModel {
+
+	public TracesTableModel(Object[][] data, String[] columns) {
+		super(data, columns);
+	}
+
+	public void pushTrace(String trace) {
+		insertRow(0, new Object[] { trace });
+	}
+}
