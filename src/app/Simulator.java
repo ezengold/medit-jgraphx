66ef431f8490164ffb2ea461f4d8a9411a10bb7a
EZENGOLD <noudekiki@gmail.com>
2021-11-03 10:44:32 +0100

    start simulation view

--- src/app/Simulator.java
+++ src/app/Simulator.java
@@ -1,21 +1,36 @@
 package app;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+
+import javax.swing.border.EmptyBorder;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 
 import models.Automata;
+import ui.Button;
+import ui.TransitionsTable;
 
 public class Simulator extends JPanel {
 
 	private static final long serialVersionUID = 8364570541969774335L;
+
+	private static String[] ACTIVES_TRANSITIONS_TABLE_COLUMNS = { "Transitions" };
 
 	private App app;
 
 	private Automata automata;
 
 	private JPanel activeTransitionsPanel;
+
+	private Object[][] activeTransitions = { { "Transition 0" }, { "Transition 1" }, { "Transition 2" } };
+
+	private TransitionsTable activeTransitionsTable;
+
+	private Button nextTransitionButton;
+
+	private Button previousTransitionButton;
 
 	private JPanel tracesPanel;
 
@@ -27,8 +42,26 @@ public class Simulator extends JPanel {
 		this.app = app;
 		this.automata = app.getAutomata();
 
-		this.activeTransitionsPanel = new JPanel();
-		this.activeTransitionsPanel.setBorder(BorderFactory.createTitledBorder(" Transitions actives "));
+		this.activeTransitionsPanel = new JPanel(new BorderLayout());
+		activeTransitionsPanel.setBorder(BorderFactory.createTitledBorder(" Transitions actives "));
+
+		this.activeTransitionsTable = new TransitionsTable(this.activeTransitions, ACTIVES_TRANSITIONS_TABLE_COLUMNS);
+		JScrollPane tableScrollPane = new JScrollPane(activeTransitionsTable);
+		tableScrollPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		activeTransitionsPanel.add(tableScrollPane, BorderLayout.CENTER);
+
+		this.nextTransitionButton = new Button("Suivant");
+		nextTransitionButton.setBackground(Color.decode("#9099ae"));
+
+		this.previousTransitionButton = new Button("Previous");
+		previousTransitionButton.setBackground(Color.decode("#9099ae"));
+
+		JPanel activesTransitionsButtonsContainer = new JPanel();
+		activesTransitionsButtonsContainer.setBorder(new EmptyBorder(0, 5, 5, 5));
+		activesTransitionsButtonsContainer.add(previousTransitionButton);
+		activesTransitionsButtonsContainer.add(nextTransitionButton);
+
+		activeTransitionsPanel.add(activesTransitionsButtonsContainer, BorderLayout.SOUTH);
 
 		this.tracesPanel = new JPanel();
 		this.tracesPanel.setBorder(BorderFactory.createTitledBorder(" Traces de simulation "));
@@ -51,7 +84,7 @@ public class Simulator extends JPanel {
 		this.apercuPanel = new JPanel();
 
 		JSplitPane mainSplit = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftSplit, apercuPanel);
-		mainSplit.setDividerLocation(550);
+		mainSplit.setDividerLocation(600);
 		mainSplit.setResizeWeight(1);
 		mainSplit.setDividerSize(3);
 		mainSplit.setBorder(null);
@@ -82,6 +115,30 @@ public class Simulator extends JPanel {
 
 	public void setActiveTransitionsPanel(JPanel activeTransitionsPanel) {
 		this.activeTransitionsPanel = activeTransitionsPanel;
+	}
+
+	public TransitionsTable getActiveTransitionsTable() {
+		return activeTransitionsTable;
+	}
+
+	public void setActiveTransitionsTable(TransitionsTable activeTransitionsTable) {
+		this.activeTransitionsTable = activeTransitionsTable;
+	}
+
+	public Button getNextTransitionButton() {
+		return nextTransitionButton;
+	}
+
+	public void setNextTransitionButton(Button nextTransitionButton) {
+		this.nextTransitionButton = nextTransitionButton;
+	}
+
+	public Button getPreviousTransitionButton() {
+		return previousTransitionButton;
+	}
+
+	public void setPreviousTransitionButton(Button previousTransitionButton) {
+		this.previousTransitionButton = previousTransitionButton;
 	}
 
 	public JPanel getTracesPanel() {
